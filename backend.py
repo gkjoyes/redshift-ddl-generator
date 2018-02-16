d7267dccec782097c90197e5a25c7fec48be1237
Georgekutty Joyes <georgekutty@hifx.co.in>
2018-02-16 18:49:49 +0530

    modification on progress

--- backend.py
+++ backend.py
@@ -1,32 +1,35 @@
-"""all database connection and data processing is done here """
+"""database connections and data processing"""
 
 import sys
 from mysql.connector import MySQLConnection, Error, errorcode
 
 
-# ------------ mysql connection, read mysql table description is done here..
 def read_mysql_table_description(creds, logging):
-    """Reading description of mysql table done here"""
+    """reading description of mysql table"""
 
-    logging.info("#Step : Reading table description is start")
+    logging.info("#Step: Reading table description is start")
     try:
         conn = MySQLConnection(
-            user=creds["mysql"]["user"], password=creds["mysql"]["pass"], host=creds["mysql"]["host"], database=creds["mysql"]["db"])
+            user=creds["mysql"]["user"],
+            password=creds["mysql"]["pass"],
+            host=creds["mysql"]["host"],
+            database=creds["mysql"]["db"])
+
         if conn.is_connected():
-            logging.info("#Step : Mysql connection established successfully")
+            logging.info("#Step: Mysql connection is established")
 
             cursor = conn.cursor()
-            cursor.execute("SHOW COLUMNS FROM {}".format(creds["mysql"]["table"]))
+            cursor.execute("SHOW COLUMNS FROM {}".format(
+                creds["mysql"]["table"]))
             return cursor.fetchall()
 
     except Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            logging.error(
-                '#Error : Something is wrong with your user name or password')
+            logging.error('#Error: Incorrect username or password')
         elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            logging.error('#Error : Database does not exist')
+            logging.error('#Error: Database does not exist')
         else:
-            logging.error('#Error : %s', err)
+            logging.error('#Error: %s', err)
 
         sys.exit(0)
 
--- main.py
+++ main.py
@@ -6,38 +6,37 @@ import sys
 from backend import read_mysql_table_description
 from core import generate_ddl
 
-# ------------- logging level..
+
+# ----files
+CONF_FILE = "config/conf.json"
+MAPPING_FILE = "config/data_type_mapping.json"
+
+
+# ----logging
 LOG_FILENAME = 'work.log'
 logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
 logging.getLogger().addHandler(logging.StreamHandler())
 
 
-# ------------- read configuration file..
 def read_conf(conf_file):
-    """Reading conf done here"""
+    """reading config file"""
 
-    logging.info("#Step : Reading conf file is start")
+    logging.info("#Step: Reading conf file start")
     try:
         return json.load(open(conf_file))
     except Exception as err:
-        logging.info("#Error : "+str(err))
+        logging.info("#Error : %s", str(err))
         sys.exit(0)
 
-# ------------main steps for ddl generation..
+
 def generate_ddl_start():
-    "All steps for query generations"
+    "steps in query generator"
 
-    # ------- file names
-    conf_file = 'config/conf.json'
-    data_type_mapping = 'config/data_type_mapping.json'
-
-    #-------- processing steps
-    creds = read_conf(conf_file)
+    creds = read_conf(CONF_FILE)
     table_info = read_mysql_table_description(creds, logging)
-    generate_ddl(table_info, creds, data_type_mapping, logging)
+    generate_ddl(table_info, creds, MAPPING_FILE, logging)
 
 
-# ------------main..
+# ----main
 if __name__ == '__main__':
-
     generate_ddl_start()
